99d4340026f9b494a75c9a64b353f298c399373d
Mohsen Mahdieh <m.mahdiyeh@gmail.com>
2022-06-20 13:02:31 +1000

    changing configuration

--- pkg/utils/configurations.go
+++ pkg/utils/configurations.go
@@ -2,10 +2,7 @@ package utils
 
 import "github.com/tkanos/gonfig"
 
-type Configuration interface {
-}
-
-func LoadConfigurations[T Configuration](path string) (Configuration, error) {
+func LoadConfigurations[T any](path string) (*T, error) {
 	var c T
 
 	err := gonfig.GetConf(path, c)
