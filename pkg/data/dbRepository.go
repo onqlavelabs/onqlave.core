fd31432c88bf532cf4ac839577da7d444b1023ce
Desmond-Onqlave <desmond@onqlave.com>
2023-06-09 16:10:44 +0700

    init branch dev

--- pkg/data/dbRepository.go
+++
@@ -1,9 +0,0 @@
-package data
-
-type DbRepository[T any] interface {
-	FindOne(query interface{}, args ...interface{}) (*T, error)
-	FindAll(query interface{}, args ...interface{}) ([]T, error)
-	Insert(model *T) (*T, error)
-	Update(model *T) (*T, error)
-	Delete(model *T) (*T, error)
-}
--- pkg/utils/handlerSetup.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "github.com/labstack/echo/v4"
-
-type HandlerSetup interface {
-	SetupRoutes(groupName string, group *echo.Group) []Subscription
-}
