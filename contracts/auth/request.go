48ec08fcdd362f438970ec002fb2b433376d6ede
Desmond-Onqlave <desmond@onqlave.com>
2023-08-11 14:55:47 +0700

    feat/registration-validation-request

--- contracts/auth/request.go
+++ contracts/auth/request.go
@@ -1,13 +1,17 @@
 package auth
 
-type RegistrationStatusRequest struct {
+type RegistrationStatus struct {
 	Request RegistrationToken `json:"request" validate:"required"`
-}
-
-type LoginRequest struct {
-	LoginDetails LoginDetails `json:"login" validate:"required"`
 }
 
 type RegistrationRequest struct {
 	Registration RegistrationDetails `json:"registration" validate:"required"`
+}
+
+type RegistrationValidation struct {
+	Validation RegistrationToken `json:"validation" validate:"required"`
+}
+
+type LoginRequest struct {
+	LoginDetails LoginDetails `json:"login" validate:"required"`
 }
