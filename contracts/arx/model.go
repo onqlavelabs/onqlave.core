ef1f7c335b5bdc8363bfc35fb39ace1b726752b6
Desmond-Onqlave <119550866+Desmond-Onqlave@users.noreply.github.com>
2023-08-21 09:22:51 +0700

    feat: update arx master key response (#23)

--- contracts/arx/model.go
+++ contracts/arx/model.go
@@ -57,21 +57,24 @@ type Detail struct {
 }
 
 type ExistingWithDetail struct {
-	ACL                 acl.ACL      `json:"acl"`
-	ID                  common.ArxId `json:"id" validate:"required"`
-	Name                string       `json:"name" validate:"required"`
-	SpendLimit          uint64       `json:"spend_limit"`
-	Description         string       `json:"description" validate:"required,min=4,max=500"`
-	Purpose             string       `json:"purpose" validate:"required,max=50"`
-	Plan                string       `json:"plan" validate:"required,max=50"`
-	Provider            string       `json:"provider" validate:"required,max=50"`
-	Regions             []string     `json:"regions" validate:"required,max=5"`
-	AvailabilityMessage string       `json:"availability_message"`
-	EncryptionMethod    string       `json:"encryption_method" validate:"required,max=50"`
-	RotationCycle       string       `json:"rotation_cycle" validate:"required,max=50"`
-	Owner               string       `json:"owner" validate:"required,max=150"`
-	IsDefault           bool         `json:"is_default"`
-	Status              string       `json:"status" validate:"required,max=50"`
+	ACL                 acl.ACL             `json:"acl"`
+	ID                  common.ArxId        `json:"id" validate:"required"`
+	Name                string              `json:"name" validate:"required"`
+	SpendLimit          uint64              `json:"spend_limit"`
+	Description         string              `json:"description" validate:"required,min=4,max=500"`
+	Purpose             string              `json:"purpose" validate:"required,max=50"`
+	Plan                string              `json:"plan" validate:"required,max=50"`
+	Provider            string              `json:"provider" validate:"required,max=50"`
+	Regions             []string            `json:"regions" validate:"required,max=5"`
+	AvailabilityMessage string              `json:"availability_message"`
+	EncryptionMethod    string              `json:"encryption_method" validate:"required,max=50"`
+	RotationCycle       string              `json:"rotation_cycle" validate:"required,max=50"`
+	Owner               string              `json:"owner" validate:"required,max=150"`
+	IsDefault           bool                `json:"is_default"`
+	Status              string              `json:"status" validate:"required,max=50"`
+	NextRotation        string              `json:"next_rotation"`
+	LastRotation        string              `json:"last_rotation"`
+	MasterKeyVersions   []*MasterKeyVersion `json:"master_key_versions"`
 }
 
 type EncryptionRotationCycle struct {
@@ -181,4 +184,9 @@ type Owner struct {
 	Name   string `json:"name"`
 	Email  string `json:"email_address"`
 	Avatar string `json:"avatar"`
+}
+
+type MasterKeyVersion struct {
+	Name     string `json:"name"`
+	IsActive bool   `json:"is_active"`
 }
